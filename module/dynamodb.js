a266e5da963d0dd2bc152d9fe731a4e4e18a0476
Roy G <18429697+6645022@users.noreply.github.com>
2018-09-24 01:39:29 +0300

    Update dynamodb.js

--- module/dynamodb.js
+++ module/dynamodb.js
@@ -17,28 +17,7 @@ export class DynamoDB extends Aws{
             return error;
         }
     }
-    async query(params){
-        console.log(213123)
+    
 
-        try {
-            let response = await this.docClient.query(params).promise();
-            return response
-        }
-        catch(error) {
-            return error;
-        }
-    }
 
-    async put(){
-        var params = {
-            TableName : 'users',
-            Key: {
-                HashKey: 'userId'
-            }
-        };
-        await this.docClient.get(params, function(err, data) {
-            if (err) console.log(err);
-            else console.log(data);
-        });
-    }
 }
